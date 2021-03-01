d6f0c11376dc1494cb7917fcd386c1f4cdbcd8c6
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-03-01 11:43:55 -0600

    Intergrated Apollo Provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
-import './App.css';
-import logo from './logo.png';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+import "./App.css";
+import logo from "./logo.png";
+
+import Launches from "./components/launches"
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+})
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="App">
-      
-      <img src={logo} alt="SpaceX" style={{width: 300, display: "block", margin: "auto"}} />
+      <img
+        src={logo}
+        alt="SpaceX"
+        style={{ width: 300, display: "block", margin: "auto" }}
+      />
+      <Launches />
     </div>
+    </ApolloProvider>
   );
 }
 

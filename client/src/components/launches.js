1eb6e5312e3bb1c87d9b5deece407ca3581a328c
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-03-01 12:41:29 -0600

    added the key comp to this comp

--- client/src/components/launches.js
+++ client/src/components/launches.js
@@ -4,6 +4,7 @@ import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 
 import Mission from'./mission'
+import MissionKey from'./missionKey';
 
 const LAUNCHES_QUERY = gql
 `
@@ -22,12 +23,13 @@ export default function launches() {
     return (
         <Fragment>
             <h1 className="display-4 my-3">Launches</h1>
+            <MissionKey />
             <Query query={LAUNCHES_QUERY}>
                 {
                     ({ loading, error, data }) => {
                         if (loading) return <h4>Loading...</h4>
                         if (error) console.log(error);
-                                                
+
                         return <Fragment>
                             {
                                 data.launches.map(launch => (

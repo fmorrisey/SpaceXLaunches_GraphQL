95f75e0014bfbbd32cd044a643e50640c349090f
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-03-02 10:09:12 -0600

    Imported GQL and wrote initial Query

--- client/src/components/mission.js
+++ client/src/components/mission.js
@@ -1,5 +1,28 @@
 import React from 'react'
 
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+
+const LAUNCHES_QUERY = gql
+`   
+    query LAUNCHES_QUERY($flight_number: Int!)  {
+        launch(flight_number: $flight_number)    {
+            flight_number
+            mission_name
+            launch_year
+            launch_success
+            launch_date_local,
+            rocket {
+                rocket_id
+                rocket_name
+                rocket_type
+            }
+        }
+    }
+`
+
 export default function mission() {
     return (
         <div>
